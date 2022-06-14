34fa6d4fd99cc83d7718e2efb11d59be78e9d548
Azizul kabir lavlu <lavlu.bcse@gmail.com>
2022-06-14 17:23:49 +0600

    add user client

--- src/Component/AddUser/AddUser.js
+++ src/Component/AddUser/AddUser.js
@@ -4,12 +4,16 @@ import '../AddUser/AddUser.css';
 const AddUser = () => {
 
     const handleSubmit = event=>{
-        event.preventDefault()
+        event.preventDefault();
+        const name = event.target.name.value;
+        const email = event.target.email.value;
+        const user = {name, email}
+        console.log(user)
     }
     return (
         <div className='addUser'>
             <h1>Add Your User</h1>
-            <form onSubmit={handleSubmit()}>
+            <form onSubmit={handleSubmit}>
                 <input type="text" name='name' placeholder='name' required />
                 <input type="email" name='email' placeholder='email' required />
                 <input type="submit" name='submit' />
