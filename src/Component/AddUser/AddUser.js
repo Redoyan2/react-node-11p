a9d0ec1e655815510bf463140d73823c1ac0317c
Azizul kabir lavlu <lavlu.bcse@gmail.com>
2022-06-15 03:29:52 +0600

    form add

--- src/Component/AddUser/AddUser.js
+++ src/Component/AddUser/AddUser.js
@@ -1,23 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import '../AddUser/AddUser.css';
 
 const AddUser = () => {
+    const [users, setUsers] = useState([]);
 
-    const handleSubmit = event=>{
+
+    const handleAddUser = event =>{
         event.preventDefault();
         const name = event.target.name.value;
         const email = event.target.email.value;
-        const user = {name, email}
+        const user = {name, email};
         console.log(user)
     }
+
+    useEffect(() => {
+        fetch('http://localhost:5000/users')
+            .then(res => res.json())
+            .then(data => {
+                setUsers(data);
+            })
+    })
     return (
         <div className='addUser'>
             <h1>Add Your User</h1>
-            <form onSubmit={handleSubmit}>
-                <input type="text" name='name' placeholder='name' required />
-                <input type="email" name='email' placeholder='email' required />
-                <input type="submit" name='submit' />
+            <form onSubmit={handleAddUser}>
+                <input type="text" name="name" placeholder='Name' required />
+               
+                <input type="email" name="email" placeholder='Email' required />
+                
+                <input type="submit" value="Add User" />
             </form>
+            <div>
+                {
+                    users.map(user => <li
+                        key={user.id}
+                    >name: {user.name}</li>)
+                }
+            </div>
         </div>
     );
 };
